26e25786829fcbbfb6eb4c02130850d1a2420fa3
hsk_the_editor <85099920+theed221120@users.noreply.github.com>
2021-06-03 18:53:08 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+
+LOG_CHAT = getenv("LOG_CHAT")
+
+ARQ_API_KEY = getenv("ARQ_API_KEY")
+# Don't Change Anything Here
+ARQ_API_URL = "http://thearq.tech"
